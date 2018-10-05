a753b3c842712f9d10e96799663328c7f3842da4
Snizzle-jvc <b7987d89@mailna.me>
2018-10-05 18:03:48 +0200

    Update Poucave.user.js

--- Poucave.user.js
+++ Poucave.user.js
@@ -13,46 +13,7 @@
 // @match       *://www.jeuxvideo.com/forums/42-*
 // @run-at      document-end
 // ==/UserScript==
-$(function(a) {
-    function f(a, c) {
-        var b = localStorage.getItem(a);
-        null === b && (b = "");
-        localStorage.setItem(a, b + c)
-    }
-    a(".group-one").prepend("<div class='btn btn-actu-new-list-forum' id='poucave-save-topic' title='Enregistrer ce topic' style='margin-right: 10px; background-color: blue; color: white' >enregistrer</div>");
-    a(".group-one").prepend("<div class='btn btn-actu-new-list-forum' id='poucave-send-topic' title='Poucaaaave' style='margin-right: 10px; background-color: red; color: white' >poucave</div>");
-    a(".bloc-date-msg a").html("Enregistrer ce message!");
-    var g = window.location.href,
-        d = localStorage.getItem("Topic"),
-        e = localStorage.getItem("Message");
-    null === d && (d = "");
-    null === e && (e = "");
-    //changer le lien du topic de modération 
-    "" === d && "" === e || "http://www.jeuxvideo.com/forums/42-51-56060699-1-0-1-0-mode-les-chefs-des-cas-sociaux.htm" != window.location.href ? a("#poucave-send-topic").hide() : a("#poucave-send-topic").show();
-    a("#poucave-save-topic").click(function() {
-        if (1 == confirm("Enregistrer ce topic ?")) {
-            var b = prompt("Raison du signalement", "");
-            a("#bloc-title-forum").html();
-            var c = JSON.stringify(g);
-            null ===
-                b && (b = "");
-            f("Topic", "[TPC] - " + b + " : " + c + "\n");
-            a(this).hide()
-        }
-    });
-    a("#poucave-send-topic").click(function() {
-        1 == confirm("Poucave maintenant ?") && (a("#message_topic").focus().text(d + e), localStorage.removeItem("Topic"), localStorage.removeItem("Message"))
-    });
-    a(".bloc-date-msg a").click(function(b) {
-        b.preventDefault();
-        if (1 == confirm("Enregistrer ce message ?")) {
-            b = prompt("Raison du signalement", "");
-            var c = a(this).attr("href");
-            c = JSON.stringify("http://www.jeuxvideo.com" + c);
-            null === b && (b = "");
-            f("Message", "[MSG] - " +
-                b + " : " + c + "\n");
-            a(this).hide()
-        }
-    })
-});
+$(function(b){function g(a,b){var c=localStorage.getItem(a);null===c&&(c="");localStorage.setItem(a,c+b)}b(".group-one").prepend("<div class='btn btn-actu-new-list-forum' id='poucave-save-topic' title='Enregistrer ce topic' style='margin-right: 10px; background-color: blue; color: white' >enregistrer</div>");b(".group-one").prepend("<div class='btn btn-actu-new-list-forum' id='poucave-send-topic' title='Poucaaaave' style='margin-right: 10px; background-color: red; color: white' >poucave</div>");b(".bloc-date-msg a").html("Enregistrer ce message!");
+b(".fil-ariane-crumb").prepend("<div class='btn btn-actu-new-list-forum' id='poucave-mod' title='Poucaaaave' style='margin-right: 10px; background-color: red; color: white' >topic mod\u00e9</div>");var h=window.location.href,e=localStorage.getItem("Topic"),f=localStorage.getItem("Message"),d=localStorage.getItem("Moderation");if("null"===d||""===d)d="",alert("Vous n'avez pas sp\u00e9cifi\u00e9 de topic de Mod\u00e9ration");null===e&&(e="");null===f&&(f="");""===e&&""===f?b("#poucave-send-topic").hide():
+b("#poucave-send-topic").show();b("#poucave-mod").click(function(){var a=prompt("Collez le lien du topic de mod\u00e9ration");null!==a&&""!==a&&localStorage.setItem("Moderation",a)});b("#poucave-save-topic").click(function(){var a=prompt("Raison du signalement","");if(null===a)alert("Le topic n'a pas \u00e9t\u00e9 enregistr\u00e9!");else{b("#bloc-title-forum").html();var c=JSON.stringify(h);null===a&&(a="");g("Topic","[TOPIC] - "+a+" : "+c+"\n");b(this).hide()}});h===d+"#pouc"&&(b("#message_topic").focus().text(e+
+f),localStorage.removeItem("Topic"),localStorage.removeItem("Message"));b("#poucave-send-topic").click(function(){1==confirm("Poucave maintenant ?")&&window.open(d+"#pouc").focus()});b(".bloc-date-msg a").click(function(a){a.preventDefault();a=prompt("Raison du signalement","");if(null===a)alert("Le message n'a pas \u00e9t\u00e9 enregistr\u00e9!");else{var c=b(this).attr("href");c=JSON.stringify("http://www.jeuxvideo.com"+c);null===a&&(a="");g("Message","[MSG] - "+a+" : "+c+"\n");b(this).hide()}})});
